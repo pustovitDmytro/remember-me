d63cbbef06ef0c27f185e149e89b931feb8de7c0
pustovitDmytro <pustic@gmail.com>
2018-01-14 01:34:44 +0200

    styled filtered links

--- src/content/index.js
+++ src/content/index.js
@@ -1,6 +1,25 @@
 // eslint-disable-line
-// const BROWS = browser;
+// function BROWS = browser;
+function getLinks(){
+    const array = [];
+    const  links = document.getElementsByTagName('a');
+    for(let i=0; i<links.length; i++) {
+        array.push(links[i]);
+    }
+    return array;
+}
 
+function match({href}){
+    const re = /id/g;
+    return re.test(href);
+}
+
+function stilizy(link){
+    link.style.color = 'red';
+    link.style.fontSize =20;
+}
 // console.log('content',BROWS);
 console.log('inside content script');
 console.log('BROWSER', BROWSER);
+const links = getLinks();
+links.filter(match).forEach(stilizy);
