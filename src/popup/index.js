a1c5f4ec5dc6b80edd5caa6eb939f9f7a2212ff6
pustovitDmytro <pustic@gmail.com>
2018-01-15 01:39:03 +0200

    localstorage in popup

--- src/popup/index.js
+++ src/popup/index.js
@@ -4,5 +4,10 @@ let photo = document.getElementById("photo");
 console.log('in popup');
 photo.className=css.eye+" "+css.eye__closed;
 photo.onclick=function(){
+    console.log('clicked in popup');
+    console.log(BROWSER);
+    chrome.storage.sync.get('value', function (obj) {
+    console.log(obj);
+});
     photo.className=css.eye+" "+css.eye__opened;
 }
