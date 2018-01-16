6974850779758055dd2eef740d3c8020aaaceb55
pustovitDmytro <pustic@gmail.com>
2018-01-17 00:27:23 +0200

    react added

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,18 +1,43 @@
+/*
+ * import App from './components/App';
+ *
+ */
+import React from 'react';
+import { Component } from 'react';
+import { render } from 'react-dom';
 
-import browser from './../polifils/browser.js';
-import css from './popup.scss';
-
-const photo = document.getElementById("photo");
-photo.className=`${css.eye} ${css.eye__closed}`;
-photo.onclick=function(){
-    console.log('clicked in popup');
-    console.log(window.location.href);
-    browser.tabs.query({ active: true, currentWindow: true }, (arrayOfTabs) => {
-        const [activeTab] = arrayOfTabs;
-        console.log("activeTab", activeTab.url);
-    });
-    chrome.storage.sync.get('value', (obj) => {
-        console.log(obj);
-    });
-    photo.className=`${css.eye} ${css.eye__opened}`;
+class App extends Component {
+    render() {
+        return (
+            <div>
+                {
+                    "ssssssssssssssssssss"
+                }
+            </div>
+        );
+    }
 }
+// console.log(document.getElementById('app'));
+render(<App />, document.getElementById('app'));
+
+// /*
+//  * import browser from './../polifils/browser.js';
+//  * import css from './popup.scss';
+//  */
+
+// /*
+//  * const photo = document.getElementById("photo");
+//  * photo.className=`${css.eye} ${css.eye__closed}`;
+//  * photo.onclick=function(){
+//  *     console.log('clicked in popup');
+//  *     console.log(window.location.href);
+//  *     browser.tabs.query({ active: true, currentWindow: true }, (arrayOfTabs) => {
+//  *         const [activeTab] = arrayOfTabs;
+//  *         console.log("activeTab", activeTab.url);
+//  *     });
+//  *     chrome.storage.sync.get('value', (obj) => {
+//  *         console.log(obj);
+//  *     });
+//  *     photo.className=`${css.eye} ${css.eye__opened}`;
+//  * }
+//  */
--- webpack/babel.js
+++ webpack/babel.js
@@ -9,10 +9,7 @@ module.exports = function() {
                     test: /\.js$/,
                     exclude: /(node_modules|bower_components)/,
                     use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['env']
-                        }
+                        loader: 'babel-loader'
                     }
                 }
             ]
